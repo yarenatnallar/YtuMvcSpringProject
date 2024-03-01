d8972d4f629c05ed69cca5d05afc62e8662137b3
asus <asus@LAPTOP-UTD469JQ>
2024-03-01 16:32:49 +0300

    Add project files.

---
+++ W01/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace W01
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
